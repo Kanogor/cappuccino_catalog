652c8b4034de5c6ce7ec947ce6433ce0e97f35a7
kanogor <the.kanogor@yandex.ru>
2024-07-28 02:01:07 +0300

    continue work with SingleCapp

--- app/src/main/java/ru/kanogor/cappuccinocatalog/presentation/singlecappuccino/SingleCapp.kt
+++ app/src/main/java/ru/kanogor/cappuccinocatalog/presentation/singlecappuccino/SingleCapp.kt
@@ -1,8 +1,8 @@
 package ru.kanogor.cappuccinocatalog.presentation.singlecappuccino
 
-import android.widget.EditText
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,31 +10,34 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import ru.kanogor.cappuccinocatalog.R
+import ru.kanogor.cappuccinocatalog.presentation.ui.theme.SwitchGradientEnd
+import ru.kanogor.cappuccinocatalog.presentation.ui.theme.SwitchGradientStart
+import ru.kanogor.cappuccinocatalog.presentation.ui.theme.TextFieldColor
 import ru.kanogor.cappuccinocatalog.presentation.ui.theme.TextFieldTextColor
 import ru.kanogor.cappuccinocatalog.presentation.ui.theme.TitleTextColor
 import ru.kanogor.cappuccinocatalog.presentation.ui.theme.ToolbarBtnColor
@@ -47,9 +50,31 @@ fun SingleCapp(onNavigateToSingleCapp: () -> Unit) {
     val costText = remember {
         mutableStateOf("")
     }
+    val switchChecked = remember {
+        mutableStateOf(false)
+    }
+    val switchGradient = Brush.radialGradient(listOf(SwitchGradientStart, SwitchGradientEnd))
+    val fieldTextColor = OutlinedTextFieldDefaults.colors(
+        unfocusedBorderColor = Color.Transparent,
+        focusedBorderColor = Color.Transparent,
+        focusedContainerColor = TextFieldColor,
+        unfocusedContainerColor = TextFieldColor,
+        focusedTextColor = TextFieldTextColor,
+        unfocusedTextColor = TextFieldTextColor,
+        cursorColor = TextFieldTextColor,
+        focusedSuffixColor = TextFieldTextColor,
+        unfocusedSuffixColor = TextFieldTextColor,
+        disabledContainerColor = ToolbarBtnColor
+    )
+
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.CenterEnd) {
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Column {
+            Column(
+                modifier = Modifier
+                    .padding(vertical = 24.dp)
+                    .verticalScroll(rememberScrollState())
+                    .weight(1f, false)
+            ) {
                 Text(
                     text = "Наименование",
                     modifier = Modifier.padding(bottom = 12.dp),
@@ -60,16 +85,9 @@ fun SingleCapp(onNavigateToSingleCapp: () -> Unit) {
                     onValueChange = {
                         nameText.value = it
                     },
+                    modifier = Modifier.width(418.dp),
                     shape = RoundedCornerShape(12.dp),
-                    colors = OutlinedTextFieldDefaults.colors(
-                        unfocusedBorderColor = Color.Transparent,
-                        focusedBorderColor = Color.Transparent,
-                        focusedContainerColor = ToolbarBtnColor,
-                        unfocusedContainerColor = ToolbarBtnColor,
-                        focusedTextColor = TextFieldTextColor,
-                        unfocusedTextColor = TextFieldTextColor,
-                        cursorColor = TextFieldTextColor,
-                    ),
+                    colors = fieldTextColor,
                 )
                 Text(
                     text = "Цена",
@@ -78,28 +96,44 @@ fun SingleCapp(onNavigateToSingleCapp: () -> Unit) {
                 )
                 OutlinedTextField(value = costText.value, onValueChange = {
                     costText.value = it
-                }, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                },
+                    modifier = Modifier.width(418.dp),
+                    enabled = !switchChecked.value,
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
                     shape = RoundedCornerShape(12.dp),
-                    colors = OutlinedTextFieldDefaults.colors(
-                        unfocusedBorderColor = Color.Transparent,
-                        focusedBorderColor = Color.Transparent,
-                        focusedContainerColor = ToolbarBtnColor,
-                        unfocusedContainerColor = ToolbarBtnColor,
-                        focusedTextColor = TextFieldTextColor,
-                        unfocusedTextColor = TextFieldTextColor,
-                        cursorColor = TextFieldTextColor,
-                        focusedSuffixColor = TextFieldTextColor,
-                        unfocusedSuffixColor = TextFieldTextColor,
-                    ),
-                    suffix = { Text(text = "P") })
+                    colors = fieldTextColor,
+                    suffix = { Text(text = "₽") })
                 Card(
                     border = BorderStroke(width = 1.dp, color = ToolbarBtnColor),
                     colors = CardDefaults.cardColors(
                         containerColor = Color.Black
-                    )
+                    ),
+                    modifier = Modifier
+                        .width(418.dp)
+                        .padding(top = 12.dp, bottom = 64.dp)
                 ) {
-                    Row {
-                        Text(text = "sss")
+                    Row( modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "Продавать бесплатно",
+                            modifier = Modifier
+                                .padding(start = 24.dp, end = 81.dp, top = 13.dp, bottom = 13.dp),
+
+                            )
+                        Switch(
+                            checked = switchChecked.value, onCheckedChange = { checked ->
+                                switchChecked.value = checked
+                            }, modifier = Modifier.padding(end = 24.dp),
+                            colors = SwitchDefaults.colors(
+                                checkedThumbColor = Color.White,
+                                checkedTrackColor = SwitchGradientStart,
+                                uncheckedThumbColor = Color.White,
+                                uncheckedTrackColor = SwitchGradientEnd,
+                                uncheckedBorderColor = SwitchGradientEnd,
+                            )
+                        )
                     }
                 }
                 Button(onClick = { onNavigateToSingleCapp() }) {
--- app/src/main/java/ru/kanogor/cappuccinocatalog/presentation/ui/theme/Color.kt
+++ app/src/main/java/ru/kanogor/cappuccinocatalog/presentation/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package ru.kanogor.cappuccinocatalog.presentation.ui.theme
 
+import android.widget.Switch
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -13,3 +14,6 @@ val ToolbarTextColor = Color(0xFF896C61)
 val ToolbarBtnColor = Color(0xFF5A4036)
 val TextFieldTextColor = Color(0xFFFFF2ED)
 val TitleTextColor = Color(0xFFC4A79B)
+val TextFieldColor = Color(0xFF150E0C)
+val SwitchGradientStart = Color(0xFFEE4E2B)
+val SwitchGradientEnd = Color(0xFFFF9900)
